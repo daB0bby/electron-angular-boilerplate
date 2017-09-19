cf1055c0b0cf028c4968924df401e25f83fe3703
Genna Eirich <gena.eirich@gmail.com>
2017-09-19 12:45:13 +0200

    removes unused declaration

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -9,7 +9,6 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: '../dist',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   },
